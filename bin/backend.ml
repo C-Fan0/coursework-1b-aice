2f4fe8ffae6800c0689995cacf3d60113d7960b2
Seb Hay (sh4g24) <sh4g24@soton.ac.uk>
2026-03-13 10:27:54 +0000

    arg_loc complete, closes #9

--- bin/backend.ml
+++ bin/backend.ml
@@ -288,8 +288,15 @@ let compile_lbl_block fn lbl ctxt blk : elem =
    [ NOTE: the first six arguments are numbered 0 .. 5 ]
 *)
 let arg_loc (n : int) : operand =
-failwith "arg_loc not implemented"
-
+  match n with
+  | 0 -> Reg Rdi
+  | 1 -> Reg Rsi
+  | 2 -> Reg Rdx
+  | 3 -> Reg Rcx
+  | 4 -> Reg R08
+  | 5 -> Reg R09
+  | _ -> Ind3 (Imm int64.of_int(8*(n-4)),Rbp) (* stack is organised like a fifo. 0-based indexing, slides use 1 based but that's confusing. due to calling conventions, arg 6 starts at Rbp+16 *)
+  
 
 (* We suggest that you create a helper function that computes the
    stack layout for a given function declaration.
