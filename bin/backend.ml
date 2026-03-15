cd063e2f27f03e11f910fc767e2553a916a51be7
Abdullah Arafat <abdullaharafat807@gmail.com>
2026-03-15 15:09:10 +0000

    fix arg_loc stack offset

--- bin/backend.ml
+++ bin/backend.ml
@@ -107,7 +107,7 @@ let compile_operand (ctxt:ctxt) (dest:X86.operand) : Ll.operand -> ins =
   | Null    -> (Movq, [Imm (Lit 0L); dest])                          
   | Const c -> (Movq, [Imm (Lit c); dest])                          
   | Gid gid -> (Leaq, [Ind3 (Lbl (Platform.mangle gid), Rip); dest]) 
-  | Id uid  -> (Movq, [lookup ctxt.layout uid; dest])          
+  | Id uid  -> (Movq, [lookup ctxt.layout uid; dest])        
 
 
 (* compiling call  ---------------------------------------------------------- *)
@@ -319,7 +319,7 @@ let arg_loc (n : int) : operand =
   | 3 -> Reg Rcx
   | 4 -> Reg R08
   | 5 -> Reg R09
-  | _ -> Ind3 (Imm int64.of_int(8*(n-4)),Rbp) (* stack is organised like a fifo. 0-based indexing, slides use 1 based but that's confusing. due to calling conventions, arg 6 starts at Rbp+16 *)
+  | _ -> Ind3 (Lit (Int64.of_int (16 + (n-6)*8)), Rbp) (* stack is organised like a fifo. 0-based indexing, slides use 1 based but that's confusing. due to calling conventions, arg 6 starts at Rbp+16 *)
 
 
 (* We suggest that you create a helper function that computes the
